4affd60418748caba93a749e2ee4063522fd8159
bacharchater <94986013+bacharchater@users.noreply.github.com>
2021-12-07 11:50:56 +0100

    Add files via upload

--- src/Section.py
+++ src/Section.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 16 19:47:50 2017
+Created on Fri Nov 26 09:31:06 2021
 
+@author: lantrins
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 19:47:50 2017
 @author: lfoul
 """
 import OpenGL.GL as gl
@@ -52,17 +58,38 @@ class Section:
 
     # Defines the vertices and faces 
     def generate(self):
-        self.vertices = [ 
-                # Définir ici les sommets
+        #definir les sommets
+        self.vertices = [
+                [0, 0, 0 ], 
+                [0, 0, self.parameters['height']], 
+                [self.parameters['width'], 0, self.parameters['height']],
+                [self.parameters['width'], 0, 0],
+                [0,self.parameters['thickness'],0],
+           
+                [0,self.parameters['thickness'],self.parameters['height']],
+                
+                [self.parameters['width'],self.parameters['thickness'],self.parameters['height']],
+                [self.parameters['width'],self.parameters['thickness'],0]
+                
                 ]
         self.faces = [
                 # définir ici les faces
+                [0, 3, 2, 1],
+                [5, 6, 7, 4],
+                [1, 0, 4, 5],
+                
+                [4, 0, 3, 7],
+                [7, 3, 2, 6],
+                [6, 2, 1, 5]
                 ]   
 
     # Checks if the opening can be created for the object x
     def canCreateOpening(self, x):
-        # A compléter en remplaçant pass par votre code
-        pass      
+        return (self.parameters['height']>=x.getParameter('height')+x.getParameter('position')[2]-self.parameters['position'][2]
+                and x.getParameter('position')[2]>=self.parameters['position'][2]
+                and self.parameters['width']>=x.getParameter('width')+x.getParameter('position')[0]-self.parameters['position'][0]
+                and x.getParameter('position')[0]>=self.parameters['position'][0]
+                )    
         
     # Creates the new sections for the object x
     def createNewSections(self, x):
@@ -72,10 +99,38 @@ class Section:
     # Draws the edges
     def drawEdges(self):
         # A compléter en remplaçant pass par votre code
-        pass           
-                    
+        gl.glPushMatrix()
+        gl.glTranslatef(self.setParameters['position'][0],self.setParameters['position'][1],self.setParameters['position'][2])
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE) # on trace les arrets : GL_FILL
+        gl.glBegin(gl.GL_QUADS) # Tracé d’un quadrilatère
+        gl.glColor3fv([self.parameters['color'][0]*0.3,self.parameters['color'][1]*0.3,self.parameters['color'][2]*0.3]) 
+        for i in self.faces:          
+            for j in i:
+                gl.glVertex3fv(self.vertices[j])
+        gl.glEnd()
+        gl.glPopMatrix()
+
     # Draws the faces
+
     def draw(self):
         # A compléter en remplaçant pass par votre code
-        pass
+        if self.parameters['edges']:
+            self.drawEdges()
+        
+        gl.glPushMatrix()
+        gl.glTranslate(self.parameters['position'][0],self.parameters['position'][1],self.parameters['position'][2])
+
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL) # on trace les faces : GL_FILL
+        gl.glBegin(gl.GL_QUADS) # Tracé d’un quadrilatère
+        gl.glColor3fv(self.parameters['color']) 
+        for i in self.faces:
+            for j in i:
+                gl.glVertex3fv(self.vertices[j])
+            
+        gl.glEnd()
+        gl.glPopMatrix()
+            
+    
+            
+        
   
